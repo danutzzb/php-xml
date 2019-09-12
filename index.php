de7e443e7a32035e99bf45191a5674ed0b17cc34
danutzzb <45382669+danutzzb@users.noreply.github.com>
2019-09-12 20:03:06 +0300

    Rss Functional

--- index.php
+++ index.php
@@ -1,16 +1,9 @@
-
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
-        <form method="post" action="">
-           
-            <input type="url" placeholder="Enter Rss Feed URL" name="url">
-            <input type="submit" name="adress" value="Read News">
-
-        </form>
     
         <?php
            if(isset($_POST['adress'])){
@@ -18,27 +11,27 @@
                $rss = simplexml_load_file($_POST['url']); 
            
            }
-
-         ?> 
-        <form method="post" action="" name="feed">
-            <select name="feedPage">
-                <?php  foreach ($rss->channel->item as $url){
-    
-                echo "<option name='url'>$url->link</option>";}?>
-            </select>
-            <input type="submit" name="news" value="Read News">
-           
-        </form>
-        <?php
-           if(isset($_POST['news'])){
+          if(isset($_POST['news'])){
                
                $news = $_POST['feedPage'];
                $page = file_get_contents($news);
                echo $page;
                
-               
-           }
+             }
         ?>
+        <form method="post" action="" name="feed">
+            <input type="url" placeholder="Enter Rss Feed URL" name="url">
+            <input type="submit" name="adress" value="Select feed">
+            <select name="feedPage">
+                
+                <?php  foreach ($rss->channel->item as $url){
+    
+                echo "<option name='url'>$url->link</option>";}?>
+            </select>
+            <input type="submit" name="news" value="Load news">
+           
+        </form>
+        
         
         
     </body>
